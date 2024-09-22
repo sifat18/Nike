456a5fafa76271036e0b0732ba4e4f47610d5c6c
mdsifat <ibnsifat@gmail.com>
2024-09-22 23:50:57 +0600

    initial sections

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,17 @@
-export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
-}
+import React from "react";
+
+export const App = () => {
+  return (
+    <main className="relative">
+      Nav
+      <section className="xl:padding-r wide:padding-b">Hero</section>
+      <section className="padding">Popular products</section>
+      <section className="padding">Super quality</section>
+      <section className="padding-x py-10">Services</section>
+      <section className="padding">special offers</section>
+      <section className="bg-pale-blue padding">Customer reviews</section>
+      <section className="padding-x sm:py-32 py-16 w-full">Subscribe</section>
+      <section className="bg-black padding-x padding-t pb-8">Footer</section>
+    </main>
+  );
+};
