273d1727637dc3a9ab914fe68289bb764c5fa2ba
mdsifat <ibnsifat@gmail.com>
2024-09-25 23:07:34 +0600

    hero section done

--- src/Sections/Hero.jsx
+++ src/Sections/Hero.jsx
@@ -11,44 +11,49 @@ export const Hero = () => {
   return (
     <section
       id="home"
-      className="w-full flex xl:flex-row  flex-col gap-10 max-container min-h-screen "
+      className="w-full flex xl:flex-row flex-col justify-center min-h-screen gap-10 max-container"
     >
-      <div className="xl:w-2/5 relative flex flex-col  justify-center items-start w-full  max-xl:padding-x pt-28">
-        <p className="text-lg font-montserrat text-coral-red">
-          Our Summer Collection
+      <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full  max-xl:padding-x pt-28">
+        <p className="text-xl font-montserrat text-coral-red">
+          Our Summer collections
         </p>
+
         <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold">
-          <span className="relative pr-10 z-10 xl:bg-white xl:whitespace-nowrap">
-            The New Arrivals
+          <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">
+            The New Arrival
           </span>
           <br />
-          <span className="text-coral-red inline-block mt-3"> Nike</span> Shoes
+          <span className="text-coral-red inline-block mt-3">Nike</span> Shoes
         </h1>
         <p className="font-montserrat text-slate-gray text-lg leading-8 mt-6 mb-14 sm:max-w-sm">
-          Discover Stylish Nike arrivals, quality comfort and innovation for
-          your active life
+          Discover stylish Nike arrivals, quality comfort, and innovation for
+          your active life.
         </p>
-        <Button label="Show Now" icon={arrowRight} />
+
+        <Button label="Shop now" iconURL={arrowRight} />
+
         <div className="flex justify-start items-start flex-wrap w-full mt-20 gap-16">
-          {statistics?.map((i, index) => (
+          {statistics.map((stat, index) => (
             <div key={index}>
-              <p className="text-4xl font-bold font-palanquin">{i?.value}</p>
-              <p className="font-montserrat leading-7 text-slate-gray">
-                {i?.label}
+              <p className="text-4xl font-palanquin font-bold">{stat.value}</p>
+              <p className="leading-7 font-montserrat text-slate-gray">
+                {stat.label}
               </p>
             </div>
           ))}
         </div>
       </div>
-      <div className="flex-1 flex justify-center items-center xl:min-h-screen bg-primary bg-cover max-xl:py-40 bg-hero bg-center w-full">
+
+      <div className="relative flex-1 flex justify-center items-center xl:min-h-screen max-xl:py-40 bg-primary bg-hero bg-cover bg-center">
         <img
-          src={bigShoe1}
-          alt="big-shoe1"
+          src={bigShoeImg}
+          alt="shoe colletion"
           width={610}
-          height={500}
-          className="object-contain z-10 relative"
+          height={502}
+          className="object-contain relative z-10"
         />
-        <div>
+
+        <div className="flex sm:gap-6 gap-4 absolute -bottom-[5%] sm:left-[10%] max-sm:px-6">
           {shoes.map((image, index) => (
             <div key={index}>
               <ShoeCard
