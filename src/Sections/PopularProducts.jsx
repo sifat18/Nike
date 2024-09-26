98ff95105511270ea624a85498fbef3990bf4044
mdsifat <ibnsifat@gmail.com>
2024-09-26 22:11:08 +0600

    update product section

--- src/Sections/PopularProducts.jsx
+++ src/Sections/PopularProducts.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { PopularProductCard } from "../components/PopularProductCard";
+import { products } from "../utility/constants";
 
 export const PopularProducts = () => {
   return (
@@ -11,6 +13,11 @@ export const PopularProducts = () => {
           Experience top-notch quality and style with our sought-after
           selections. Discover a world of comfort, design, and value
         </p>
+      </div>
+      <div className="mt-16 grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 sm:gap-6 gap-14">
+        {products.map((product) => (
+          <PopularProductCard key={product.name} {...product} />
+        ))}
       </div>
     </section>
   );
