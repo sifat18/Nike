4c4afa5903c47b6c7a22fefc9371fd2944bbd3f4
mdsifat <ibnsifat@gmail.com>
2024-09-27 16:26:05 +0600

    services

--- src/Sections/Services.jsx
+++ src/Sections/Services.jsx
@@ -1,5 +1,11 @@
 import React from "react";
 
 export const Services = () => {
-  return <div>Services</div>;
+  return (
+    <section className="max-container flex justify-center flex-wrap gap-9">
+      {services.map((service) => (
+        <ServiceCard key={service.label} {...service} />
+      ))}
+    </section>
+  );
 };
