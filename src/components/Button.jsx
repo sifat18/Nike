961075d84407d3e40ec3345eafd247d44745620e
mdsifat <ibnsifat@gmail.com>
2024-09-27 16:14:50 +0600

    update btn component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,7 +4,14 @@ export const Button = ({ label, icon }) => {
   return (
     <button className="flex justify-center items-center px-7 gap-2 py-4 text-lg border font-montserrat leading-none bg-coral-red rounded-full text-white border-coral-red">
       {label}
-      <img src={icon} alt="" srcset="" className="ml-2 rounded-full w-5 h-5" />
+
+      {icon && (
+        <img
+          src={icon}
+          alt="arrow right icon"
+          className="ml-2 rounded-full bg-white w-5 h-5"
+        />
+      )}
     </button>
   );
 };
