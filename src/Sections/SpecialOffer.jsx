465a0afa47be92850c07f667821d78068b87f625
mdsifat <ibnsifat@gmail.com>
2024-09-28 12:44:35 +0600

    update layout stying special offer

--- src/Sections/SpecialOffer.jsx
+++ src/Sections/SpecialOffer.jsx
@@ -1,17 +1,18 @@
 import React from "react";
 import { Button } from "../components/Button";
 import { arrowRight } from "../assets/icons";
+import { offer } from "../assets/images";
 
 export const SpecialOffer = () => {
   return (
-    <section>
+    <section className="flex max-lg:flex-col-reverse">
       <div className="">
         <img
           src={offer}
           alt="Shoe Promotion"
           width={773}
           height={687}
-          // className="object-contain w-full"
+          className="object-contain w-full"
         />
       </div>
       <div>
@@ -19,17 +20,17 @@ export const SpecialOffer = () => {
           <span className="text-coral-red">Special </span>
           Offer
         </h2>
-        <p className="">
+        <p className="mt-4 info-text">
           Embark on a shopping journey that redefines your experience with
           unbeatable deals. From premier selections to incredible savings, we
           offer unparalleled value that sets us apart.
         </p>
-        <p className="">
+        <p className="mt-6 info-text">
           Navigate a realm of possibilities designed to fulfill your unique
           desires, surpassing the loftiest expectations. Your journey with us is
           nothing short of exceptional.
         </p>
-        <div className="">
+        <div className="mt-11 flex flex-wrap gap-4">
           <Button label="Shop now" iconURL={arrowRight} />
           {/* <Button
             label="Learn more"
