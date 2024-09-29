474d630086aa046efc5cd826eaaec2a8a11004e3
mdsifat <ibnsifat@gmail.com>
2024-09-29 20:28:34 +0600

    subscribe layout

--- src/Sections/Subscribe.jsx
+++ src/Sections/Subscribe.jsx
@@ -1,5 +1,19 @@
 import React from "react";
+import { Button } from "../components/Button";
 
 export const Subscribe = () => {
-  return <div>Subscribe</div>;
+  return (
+    <section id="contact-us" className="">
+      <h3 className="">
+        Sign Up for
+        <span className="text-coral-red"> Updates </span>& Newsletter
+      </h3>
+      <div className="">
+        <input type="text" placeholder="subscribe@nike.com" className="input" />
+        <div className="">
+          <Button label="Sign Up" fullWidth />
+        </div>
+      </div>
+    </section>
+  );
 };
