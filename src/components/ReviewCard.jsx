40671a25ada5745214564e404918c6933ef9a004
mdsifat <ibnsifat@gmail.com>
2024-09-29 20:18:43 +0600

    review card img design

---
+++ src/components/ReviewCard.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const ReviewCard = ({ imgURL, customerName, rating, feedback }) => {
+  return (
+    <div className="flex justify-center items-center flex-col">
+      <img
+        src={imgURL}
+        alt="customer"
+        className="rounded-full object-cover w-[120px] h-[120px]"
+      />
+      <p className="mt-6 max-w-sm text-center info-text">{feedback}</p>
+    </div>
+  );
+};
