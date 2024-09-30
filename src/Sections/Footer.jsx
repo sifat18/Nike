96a68cc3af2fe6c6c9cf483fe775c2d40cfcbd97
mdsifat <ibnsifat@gmail.com>
2024-09-30 23:32:36 +0600

    update

--- src/Sections/Footer.jsx
+++ src/Sections/Footer.jsx
@@ -5,8 +5,8 @@ import { socialMedia } from "../utility/constants";
 export const Footer = () => {
   return (
     <footer className="max-container">
-      <div className="">
-        <div className="">
+      <div className="flex justify-between items-start gap-20 flex-wrap max-lg:flex-col">
+        <div className="mt-6 text-base leading-7 font-montserrat text-white-400 sm:max-w-sm">
           <a href="/">
             <img
               src={footerLogo}
@@ -16,13 +16,16 @@ export const Footer = () => {
               className="m-0"
             />
           </a>
-          <p className="">
+          <p className="mt-6 text-base leading-7 font-montserrat text-white-400 sm:max-w-sm">
             Get shoes ready for the new term at your nearest Nike store. Find
             Your perfect Size In Store. Get Rewards
           </p>
-          <div className="">
+          <div className="flex items-center gap-5 mt-8">
             {socialMedia.map((icon) => (
-              <div className="" key={icon.alt}>
+              <div
+                className="flex justify-center items-center w-12 h-12 bg-white rounded-full"
+                key={icon.alt}
+              >
                 <img src={icon.src} alt={icon.alt} width={24} height={24} />
               </div>
             ))}
