7b303fdf13ed823d7917f78e21451476324ded14
mdsifat <ibnsifat@gmail.com>
2024-09-30 23:28:41 +0600

    footer logo and text

--- src/Sections/Footer.jsx
+++ src/Sections/Footer.jsx
@@ -1,5 +1,34 @@
 import React from "react";
+import { footerLogo } from "../assets/images";
+import { socialMedia } from "../utility/constants";
 
 export const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="max-container">
+      <div className="">
+        <div className="">
+          <a href="/">
+            <img
+              src={footerLogo}
+              alt="logo"
+              width={150}
+              height={46}
+              className="m-0"
+            />
+          </a>
+          <p className="">
+            Get shoes ready for the new term at your nearest Nike store. Find
+            Your perfect Size In Store. Get Rewards
+          </p>
+          <div className="">
+            {socialMedia.map((icon) => (
+              <div className="" key={icon.alt}>
+                <img src={icon.src} alt={icon.alt} width={24} height={24} />
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </footer>
+  );
 };
