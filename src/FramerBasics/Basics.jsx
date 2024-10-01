600c1a9dd0de907d2474158c421c749f549e11bb
mdsifat <ibnsifat@gmail.com>
2024-10-01 23:57:52 +0600

    update stagger setup

--- src/FramerBasics/Basics.jsx
+++ src/FramerBasics/Basics.jsx
@@ -1,28 +1,37 @@
 import React from "react";
-
+import { motion } from "framer-motion";
 export const Basics = () => {
   return (
-    <section className="flex flex-col overflow-x-hidden gap-10">
-      <div className="grid grid-cols-3 gap-10 p-10">
-        <div className="bg-slate-gray rounded-lg aspect-square flex justify-center items-center">
+    <section className="flex flex-col overflow-x-hidden space-y-5">
+      <motion.div
+        variants={{
+          hidden: { opacity: 0 },
+          show: { opacity: 1 },
+          transition: { staggerChildren: 0.25 },
+        }}
+        initial="hidden"
+        animate="show"
+        className="grid grid-cols-3 gap-10 p-10"
+      >
+        <div className="bg-slate-gray rounded-lg aspect-square gap-10 flex justify-center items-center">
           1
         </div>
-        <div className="bg-slate-gray rounded-lg aspect-square flex justify-center items-center">
+        <div className="bg-slate-gray rounded-lg aspect-square gap-10 flex justify-center items-center">
           1
         </div>
-        <div className="bg-slate-gray rounded-lg aspect-square flex justify-center items-center">
+        <div className="bg-slate-gray rounded-lg aspect-square gap-10 flex justify-center items-center">
           1
         </div>
-        <div className="bg-slate-gray rounded-lg aspect-square flex justify-center items-center">
+        <div className="bg-slate-gray rounded-lg aspect-square gap-10 flex justify-center items-center">
           1
         </div>
-        <div className="bg-slate-gray rounded-lg aspect-square flex justify-center items-center">
+        <div className="bg-slate-gray rounded-lg aspect-square gap-10 flex justify-center items-center">
           1
         </div>
-        <div className="bg-slate-gray rounded-lg aspect-square flex justify-center items-center">
+        <div className="bg-slate-gray rounded-lg aspect-square gap-10 flex justify-center items-center">
           1
         </div>
-      </div>
+      </motion.div>
     </section>
   );
 };
