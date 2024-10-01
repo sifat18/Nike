4723f8a641482840df91b63e3bdafa510053105f
mdsifat <ibnsifat@gmail.com>
2024-10-02 00:05:06 +0600

    update fix stagger

--- src/FramerBasics/Basics.jsx
+++ src/FramerBasics/Basics.jsx
@@ -6,29 +6,46 @@ export const Basics = () => {
       <motion.div
         variants={{
           hidden: { opacity: 0 },
-          show: { opacity: 1 },
-          transition: { staggerChildren: 0.25 },
+          show: { opacity: 1, transition: { staggerChildren: 1 } },
         }}
         initial="hidden"
         animate="show"
         className="grid grid-cols-3 gap-10 p-10"
       >
-        <div className="bg-slate-gray rounded-lg aspect-square gap-10 flex justify-center items-center">
+        <motion.div
+          variants={{
+            hidden: { opacity: 0 },
+            show: { opacity: 1 },
+          }}
+          className="bg-slate-800 rounded-lg aspect-square gap-10 flex justify-center items-center"
+        >
+          1
+        </motion.div>
+        <motion.div
+          variants={{
+            hidden: { opacity: 0 },
+            show: { opacity: 1 },
+          }}
+          className="bg-slate-800 rounded-lg aspect-square gap-10 flex justify-center items-center"
+        >
+          1
+        </motion.div>
+        <motion.div
+          variants={{
+            hidden: { opacity: 0 },
+            show: { opacity: 1 },
+          }}
+          className="bg-slate-800 rounded-lg aspect-square gap-10 flex justify-center items-center"
+        >
+          1
+        </motion.div>
+        <div className="bg-slate-800 rounded-lg aspect-square gap-10 flex justify-center items-center">
           1
         </div>
-        <div className="bg-slate-gray rounded-lg aspect-square gap-10 flex justify-center items-center">
+        <div className="bg-slate-800 rounded-lg aspect-square gap-10 flex justify-center items-center">
           1
         </div>
-        <div className="bg-slate-gray rounded-lg aspect-square gap-10 flex justify-center items-center">
-          1
-        </div>
-        <div className="bg-slate-gray rounded-lg aspect-square gap-10 flex justify-center items-center">
-          1
-        </div>
-        <div className="bg-slate-gray rounded-lg aspect-square gap-10 flex justify-center items-center">
-          1
-        </div>
-        <div className="bg-slate-gray rounded-lg aspect-square gap-10 flex justify-center items-center">
+        <div className="bg-slate-800 rounded-lg aspect-square gap-10 flex justify-center items-center">
           1
         </div>
       </motion.div>
