7b9aaafb3b99bf7cfbf409d4acea5ec25ed3aca6
mdsifat <ibnsifat@gmail.com>
2024-10-02 23:25:52 +0600

    update shape shift  anime

--- src/FramerBasics/Basics.jsx
+++ src/FramerBasics/Basics.jsx
@@ -21,13 +21,39 @@ export const Basics = () => {
           variants={blinkForChild}
           className="bg-slate-800 rounded-lg aspect-square gap-10 flex justify-center items-center"
         >
-          1
+          {/* fade from right left */}
+          <motion.div
+            initial={{ opacity: 0, x: -50 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 2, delay: 0.2, ease: "easeInOut" }}
+            className="bg-red-500 w-5 h-5 rounded"
+          ></motion.div>
+          <motion.div
+            initial={{ opacity: 0, x: 50 }}
+            animate={{ opacity: 1, x: 0 }}
+            transition={{ duration: 1, delay: 0.4, ease: "easeInOut" }}
+            className="bg-green-400 w-5 h-5 rounded-full"
+          ></motion.div>
         </motion.div>
         <motion.div
           variants={blinkForChild}
           className="bg-slate-800 rounded-lg aspect-square gap-10 flex justify-center items-center"
         >
-          1
+          {/* shape shift */}
+          <motion.div
+            animate={{
+              scale: [1, 2, 1, 2, 1],
+              rotate: [0, -45, 90, -135, 180],
+              borderRadius: ["10%", "10%", "10%", "50%", "10%"],
+            }}
+            transition={{
+              duration: 5,
+              // delay: 0.2,
+              ease: "easeInOut",
+              repeat: Infinity,
+            }}
+            className="bg-red-500 w-1/3 h-1/3 rounded"
+          ></motion.div>
         </motion.div>
         <motion.div
           variants={blinkForChild}
