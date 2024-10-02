341a00e3a8547d7a5fd946dc54f6314d8c2fb44e
mdsifat <ibnsifat@gmail.com>
2024-10-02 22:56:26 +0600

    update blink anime

--- src/FramerBasics/Basics.jsx
+++ src/FramerBasics/Basics.jsx
@@ -1,53 +1,58 @@
 import React from "react";
 import { motion } from "framer-motion";
+const blinkForParent = {
+  hidden: { opacity: 0 },
+  show: { opacity: 1, transition: { staggerChildren: 1 } },
+};
+const blinkForChild = {
+  hidden: { opacity: 0 },
+  show: { opacity: 1 },
+};
 export const Basics = () => {
   return (
     <section className="flex flex-col overflow-x-hidden space-y-5">
       <motion.div
-        variants={{
-          hidden: { opacity: 0 },
-          show: { opacity: 1, transition: { staggerChildren: 1 } },
-        }}
+        variants={blinkForParent}
         initial="hidden"
         animate="show"
         className="grid grid-cols-3 gap-10 p-10"
       >
         <motion.div
-          variants={{
-            hidden: { opacity: 0 },
-            show: { opacity: 1 },
-          }}
+          variants={blinkForChild}
           className="bg-slate-800 rounded-lg aspect-square gap-10 flex justify-center items-center"
         >
           1
         </motion.div>
         <motion.div
-          variants={{
-            hidden: { opacity: 0 },
-            show: { opacity: 1 },
-          }}
+          variants={blinkForChild}
           className="bg-slate-800 rounded-lg aspect-square gap-10 flex justify-center items-center"
         >
           1
         </motion.div>
         <motion.div
-          variants={{
-            hidden: { opacity: 0 },
-            show: { opacity: 1 },
-          }}
+          variants={blinkForChild}
           className="bg-slate-800 rounded-lg aspect-square gap-10 flex justify-center items-center"
         >
           1
         </motion.div>
-        <div className="bg-slate-800 rounded-lg aspect-square gap-10 flex justify-center items-center">
+        <motion.div
+          variants={blinkForChild}
+          className="bg-slate-800 rounded-lg aspect-square gap-10 flex justify-center items-center"
+        >
           1
-        </div>
-        <div className="bg-slate-800 rounded-lg aspect-square gap-10 flex justify-center items-center">
+        </motion.div>
+        <motion.div
+          variants={blinkForChild}
+          className="bg-slate-800 rounded-lg aspect-square gap-10 flex justify-center items-center"
+        >
           1
-        </div>
-        <div className="bg-slate-800 rounded-lg aspect-square gap-10 flex justify-center items-center">
+        </motion.div>
+        <motion.div
+          variants={blinkForChild}
+          className="bg-slate-800 rounded-lg aspect-square gap-10 flex justify-center items-center"
+        >
           1
-        </div>
+        </motion.div>
       </motion.div>
     </section>
   );
