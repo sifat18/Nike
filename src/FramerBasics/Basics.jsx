f21844b2c3aad447979280a0c8ca0581da01c6ae
mdsifat <ibnsifat@gmail.com>
2024-10-07 23:48:22 +0600

    update svg animation

--- src/FramerBasics/Basics.jsx
+++ src/FramerBasics/Basics.jsx
@@ -8,6 +8,18 @@ const blinkForChild = {
   hidden: { opacity: 0 },
   show: { opacity: 1 },
 };
+const svgIconVariants = {
+  hidden: {
+    opacity: 0,
+    pathLength: 0,
+    fill: "rgba(252, 211, 77, 0)",
+  },
+  visible: {
+    opacity: 1,
+    pathLength: 1,
+    fill: "rgba(252, 211, 77, 1)",
+  },
+};
 export const Basics = () => {
   const { scrollYProgress } = useScroll();
   return (
@@ -115,6 +127,27 @@ export const Basics = () => {
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 24 24"
             className="stroke-orange-500 w-1/2 stroke-[0.5]"
+            initial="hidden"
+            variants={svgIconVariants}
+            animate="visible"
+            transition={{
+              default: {
+                duration: 2,
+                ease: "easeInOut",
+                delay: 1,
+                repeat: Infinity,
+                repeatType: "reverse",
+                repeatDelay: 1,
+              },
+              fill: {
+                duration: 2,
+                ease: "easeIn",
+                delay: 2,
+                repeat: Infinity,
+                repeatType: "reverse",
+                repeatDelay: 1,
+              },
+            }}
           >
             <motion.path d="m3.75 13.5 10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75Z" />
           </motion.svg>
