973c510d4f5fa5c60fd6e10789fe843e9d02d413
mdsifat <ibnsifat@gmail.com>
2024-10-07 23:46:12 +0600

    update layout svg

--- src/FramerBasics/Basics.jsx
+++ src/FramerBasics/Basics.jsx
@@ -110,7 +110,14 @@ export const Basics = () => {
           variants={blinkForChild}
           className="bg-slate-800 rounded-lg aspect-square gap-10 flex justify-center items-center"
         >
-          1
+          {/* drag shift */}
+          <motion.svg
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox="0 0 24 24"
+            className="stroke-orange-500 w-1/2 stroke-[0.5]"
+          >
+            <motion.path d="m3.75 13.5 10.5-11.25L12 10.5h8.25L9.75 21.75 12 13.5H3.75Z" />
+          </motion.svg>
         </motion.div>
       </motion.div>
     </section>
