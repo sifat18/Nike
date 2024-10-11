e85fe444ab6f3280035da358738e2ea45af839c8
mdsifat <ibnsifat@gmail.com>
2024-10-11 23:55:38 +0600

    update

--- src/Sections/Hero.jsx
+++ src/Sections/Hero.jsx
@@ -111,18 +111,26 @@ export const Hero = () => {
           />
         </AnimatePresence>
 
-        <div className="flex sm:gap-6 gap-4 absolute -bottom-[5%] sm:left-[10%] max-sm:px-6">
+        <motion.div
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1, transition: { staggerChildren: 1 } }}
+          className="flex sm:gap-6 gap-4 absolute -bottom-[5%] sm:left-[10%] max-sm:px-6"
+        >
           {shoes.map((image, index) => (
-            <div key={index}>
+            <motion.div
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              key={index}
+            >
               <ShoeCard
                 index={index}
                 imgURL={image}
                 changeBigShoeImage={(shoe) => setBigShoeImg(shoe)}
                 bigShoeImg={bigShoeImg}
               />
-            </div>
+            </motion.div>
           ))}
-        </div>
+        </motion.div>
       </div>
     </section>
   );
