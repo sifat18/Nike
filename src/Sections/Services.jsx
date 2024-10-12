5ffde9ebaef53f42e80b45a59feffdbd7d7b25f4
mdsifat <ibnsifat@gmail.com>
2024-10-12 16:53:47 +0600

    update service card

--- src/Sections/Services.jsx
+++ src/Sections/Services.jsx
@@ -5,8 +5,8 @@ import { ServiceCard } from "../components/ServiceCard";
 export const Services = () => {
   return (
     <section className="max-container flex justify-center flex-wrap gap-9">
-      {services.map((service) => (
-        <ServiceCard key={service.label} {...service} />
+      {services.map((service, index) => (
+        <ServiceCard key={service.label} {...service} index={index} />
       ))}
     </section>
   );
--- src/components/ServiceCard.jsx
+++ src/components/ServiceCard.jsx
@@ -1,8 +1,23 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import { motion, useAnimation, useInView } from "framer-motion";
 
-export const ServiceCard = ({ imgURL, label, subtext }) => {
+export const ServiceCard = ({ imgURL, label, subtext, index }) => {
+  const containerRef = useRef(null);
+  const isInView = useInView(containerRef, { once: false, amount: "all" });
+  const mainControls = useAnimation();
+  useEffect(() => {
+    isInView && mainControls.start("show");
+  }, [isInView]);
+
   return (
-    <div className="flex-1 sm:w-[350px] sm:min-w-[350px] w-full rounded-[20px] shadow-3xl px-10 py-16">
+    <motion.div
+      ref={containerRef}
+      variants={{ hidden: { opacity: 0 }, show: { opacity: 1 } }}
+      initial="hidden"
+      animate={mainControls}
+      transition={{ delay: index / 2, duration: 0.5 }}
+      className="flex-1 sm:w-[350px] sm:min-w-[350px] w-full rounded-[20px] shadow-3xl px-10 py-16"
+    >
       <div className="w-11 h-11 flex justify-center items-center bg-coral-red rounded-full">
         <img src={imgURL} alt={label} width={24} height={24} />
       </div>
@@ -12,6 +27,6 @@ export const ServiceCard = ({ imgURL, label, subtext }) => {
       <p className="mt-3 break-words font-montserrat text-lg leading-normal text-slate-gray">
         {subtext}
       </p>
-    </div>
+    </motion.div>
   );
 };
