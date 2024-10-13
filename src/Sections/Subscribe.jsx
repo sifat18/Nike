022e3b03d405b0124891e8e1ce21feb9734ab388
mdsifat <ibnsifat@gmail.com>
2024-10-13 12:01:12 +0600

    update subscribe

--- src/Sections/Subscribe.jsx
+++ src/Sections/Subscribe.jsx
@@ -1,25 +1,37 @@
 import React, { useRef } from "react";
 import { Button } from "../components/Button";
-import { motion, useScroll } from "framer-motion";
+import { motion, useScroll, useTransform } from "framer-motion";
 
 export const Subscribe = () => {
-  null;
-
+  const containerRef = useRef(null);
+  const { scrollYProgress } = useScroll({
+    target: containerRef,
+    offset: ["start end", "end end"],
+  });
+  const right = useTransform(scrollYProgress, [0, 1], ["-50%", "0%"]);
+  const left = useTransform(scrollYProgress, [0, 1], ["50%", "0%"]);
   return (
     <section
+      ref={containerRef}
       id="contact-us"
       className="max-container flex justify-between items-center max-lg:flex-col gap-10"
     >
-      <h3 className="text-4xl leading-[68px] lg:max-w-md font-palanquin font-bold">
+      <motion.h3
+        style={{ translateX: right }}
+        className="text-4xl leading-[68px] lg:max-w-md font-palanquin font-bold"
+      >
         Sign Up for
         <span className="text-coral-red"> Updates </span>& Newsletter
-      </h3>
-      <div className="lg:max-w-[40%] w-full flex items-center max-sm:flex-col gap-5 p-2.5 sm:border sm:border-slate-gray rounded-full">
+      </motion.h3>
+      <motion.div
+        style={{ translateX: left }}
+        className="lg:max-w-[40%] w-full flex items-center max-sm:flex-col gap-5 p-2.5 sm:border sm:border-slate-gray rounded-full"
+      >
         <input type="text" placeholder="subscribe@nike.com" className="input" />
         <div className="flex max-sm:justify-end items-center max-sm:w-full">
           <Button label="Sign Up" fullWidth />
         </div>
-      </div>
+      </motion.div>
     </section>
   );
 };
