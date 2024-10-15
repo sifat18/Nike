c6d56af8e0b596107c9aaf9a5196fef4f48ea355
mdsifat <ibnsifat@gmail.com>
2024-10-15 23:03:23 +0600

    update

---
+++ src/FramerBasics/Parallax.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export const Parallax = () => {
+  return (
+    <main className="relative">
+      <Section1 />
+
+      <Section2 />
+    </main>
+  );
+};
---
+++ src/FramerBasics/Section2.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export const Section2 = () => {
+  return <div>Section2</div>;
+};
