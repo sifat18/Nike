1c35eb24a9ec7b50fbf706c4e9bf1c2061131882
mdsifat <ibnsifat@gmail.com>
2024-10-18 22:30:33 +0600

    update

--- src/FramerBasics/Section2.jsx
+++ src/FramerBasics/Section2.jsx
@@ -5,7 +5,7 @@ import { useTransform, motion } from "framer-motion";
 export const Section2 = ({ scrollYProgress }) => {
   const scale = useTransform(scrollYProgress, [0, 1], [0.8, 1]);
 
-  const rotate = useTransform(scrollYProgress, [0, 1], [5, 0]);
+  const rotate = useTransform(scrollYProgress, [0, 1], [2, 0]);
   return (
     <motion.div style={{ scale, rotate }} className="relative h-screen">
       <img src={Pic2} alt="img" placeholder="blur" fill />
