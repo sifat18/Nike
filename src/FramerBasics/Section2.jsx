c13737c4550892772f0ee648ead334659292f8ad
mdsifat <ibnsifat@gmail.com>
2024-10-21 23:52:28 +0600

    update

--- src/FramerBasics/Section2.jsx
+++ src/FramerBasics/Section2.jsx
@@ -1,14 +1,14 @@
-import React from "react";
-import Pic2 from "../assets/images/2.jpeg";
-import { useTransform, motion } from "framer-motion";
+// import React from "react";
+// import Pic2 from "../assets/images/2.jpeg";
+// import { useTransform, motion } from "framer-motion";
 
-export const Section2 = ({ scrollYProgress }) => {
-  const scale = useTransform(scrollYProgress, [0, 1], [0.8, 1]);
+// export const Section2 = ({ scrollYProgress }) => {
+//   const scale = useTransform(scrollYProgress, [0, 1], [0.8, 1]);
 
-  const rotate = useTransform(scrollYProgress, [0, 1], [2, 0]);
-  return (
-    <motion.div style={{ scale, rotate }} className="relative h-screen">
-      <img src={Pic2} alt="img" placeholder="blur" fill />
-    </motion.div>
-  );
-};
+//   const rotate = useTransform(scrollYProgress, [0, 1], [2, 0]);
+//   return (
+//     <motion.div style={{ scale, rotate }} className="relative h-screen">
+//       <img src={Pic2} alt="img" placeholder="blur" fill />
+//     </motion.div>
+//   );
+// };
